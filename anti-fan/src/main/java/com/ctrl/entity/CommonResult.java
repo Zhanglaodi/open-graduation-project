586fe8d758e5d9b3b9fd0efced0b163cf9f0c874
zxt <1535852024@qq.com>
2023-05-06 10:48:51 +0800

    引入lombok以及是实现返回类

---
+++ anti-fan/src/main/java/com/ctrl/entity/CommonResult.java
@@ -0,0 +1,44 @@
+package com.ctrl.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The type Common result.
+ *
+ * @param <T> the type parameter
+ * @author dalaodi
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    /**
+     * Ok common result. 成功返回
+     *
+     * @param <T>     the type parameter
+     * @param message the message
+     * @param data    the data
+     * @return the common result
+     */
+    public static <T> CommonResult<T> ok(String message, T data) {
+        return new CommonResult<>(200, message, data);
+    }
+
+    /**
+     * Error common result.
+     *
+     * @param <T>     the type parameter
+     * @param code    the code
+     * @param message the message
+     * @return the common result
+     */
+    public static <T> CommonResult<T> error(Integer code, String message) {
+        return new CommonResult<>(code, message, null);
+    }
+}
