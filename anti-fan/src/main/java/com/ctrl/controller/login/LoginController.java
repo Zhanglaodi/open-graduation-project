8502e8b0efe8fda3a0be9e19edc4094e663c3fbc
zxt <1535852024@qq.com>
2023-05-06 13:39:28 +0800

    在登录控制器中添加@sl4j注解

--- anti-fan/src/main/java/com/ctrl/controller/login/LoginController.java
+++ anti-fan/src/main/java/com/ctrl/controller/login/LoginController.java
@@ -1,6 +1,7 @@
 package com.ctrl.controller.login;
 
 import com.ctrl.entity.CommonResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping(value = "/api/login")
 @RestController
+@Slf4j
 public class LoginController {
     /**
      * Login common result.登录方法
@@ -19,6 +21,7 @@ public class LoginController {
      */
     @RequestMapping(value = "/login")
     public CommonResult<String> login() {
+        log.info("登录成功");
         return CommonResult.ok("登录成功", null);
     }
 }
