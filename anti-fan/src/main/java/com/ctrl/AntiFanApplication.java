901c5290a96235d5cfecff35fa61d51fbae9e87f
zxt <1535852024@qq.com>
2023-05-06 10:37:52 +0800

    添加主启动类

---
+++ anti-fan/src/main/java/com/ctrl/AntiFanApplication.java
@@ -0,0 +1,24 @@
+package com.ctrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Anti fan application.
+ *
+ * @author zhanglaodi
+ * @version 1.0
+ * @date 2023年5月6日10点36分
+ * @description 主启动类
+ */
+@SpringBootApplication
+public class AntiFanApplication {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFanApplication.class, args);
+    }
+}
