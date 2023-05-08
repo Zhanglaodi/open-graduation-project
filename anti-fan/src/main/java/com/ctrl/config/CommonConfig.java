31b51409b4543ff353297e696cdc27b534f2f4cb
zxt <1535852024@qq.com>
2023-05-08 10:05:22 +0800

    添加配置类

---
+++ anti-fan/src/main/java/com/ctrl/config/CommonConfig.java
@@ -0,0 +1,12 @@
+package com.ctrl.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The type Common config.
+ *
+ * @author dalaodi
+ */
+@Configuration
+public class CommonConfig {
+}
