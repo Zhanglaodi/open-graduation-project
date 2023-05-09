c6efaeff517dd72e0af5c04f86b99957d56526fb
zxt <1535852024@qq.com>
2023-05-09 09:17:49 +0800

    登陆控制器添加验证码接口

--- anti-fan/src/main/java/com/ctrl/controller/login/LoginController.java
+++ anti-fan/src/main/java/com/ctrl/controller/login/LoginController.java
@@ -1,7 +1,9 @@
 package com.ctrl.controller.login;
 
 import com.ctrl.entity.CommonResult;
+import com.ctrl.utils.CaptchaUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,17 @@ public class LoginController {
     public CommonResult<String> login() {
         log.info("登录成功");
         return CommonResult.ok("登录成功", null);
+    }
+
+
+    /**
+     * Gets captcha.获取验证码 当然不能让验证码被无限制的获取 令牌
+     *
+     * @return the captcha
+     */
+    @GetMapping(value = "/get_captcha")
+    public CommonResult<CaptchaUtil.Captcha> getCaptcha() {
+        CaptchaUtil.Captcha generate = CaptchaUtil.generate(4);
+        return CommonResult.ok("获取验证码成功", generate);
     }
 }
