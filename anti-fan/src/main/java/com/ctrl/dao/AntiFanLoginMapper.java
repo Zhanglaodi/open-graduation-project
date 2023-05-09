d2b5d879673af37608c94685e9024aa9aa34f049
zxt <1535852024@qq.com>
2023-05-09 10:38:16 +0800

    登录mapper

---
+++ anti-fan/src/main/java/com/ctrl/dao/AntiFanLoginMapper.java
@@ -0,0 +1,14 @@
+package com.ctrl.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ctrl.entity.user.UsersDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * The interface Anti fan login mapper.
+ *
+ * @author dalaodi
+ */
+@Mapper
+public interface AntiFanLoginMapper extends BaseMapper<UsersDO> {
+}
