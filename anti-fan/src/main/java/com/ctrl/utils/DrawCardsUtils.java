f0ddd62d93ac40553bc84d10aaf8498068a98445
zxt <1535852024@qq.com>
2023-05-19 08:36:34 +0800

    添加抽卡工具类

---
+++ anti-fan/src/main/java/com/ctrl/utils/DrawCardsUtils.java
@@ -0,0 +1,40 @@
+package com.ctrl.utils;
+
+import java.util.Random;
+
+/**
+ * The type Draw cards utils.
+ */
+public class DrawCardsUtils {
+    private static final double COMMON_PROBABILITY = 0.5069;
+    private static final double ELITE_PROBABILITY = 0.436;
+    private static final double LIMITED_PROBABILITY = 0.047;
+    private static final double SP_PROBABILITY = 0.001;
+    private final Random random;
+
+    /**
+     * Instantiates a new Draw cards utils.
+     */
+    public DrawCardsUtils() {
+        this.random = new Random();
+    }
+
+    /**
+     * Draw card int.
+     *
+     * @return the int
+     */
+    public int drawCard() {
+        double randomValue = random.nextDouble();
+        if (randomValue < COMMON_PROBABILITY) {
+            return 1;
+        } else if (randomValue < COMMON_PROBABILITY + ELITE_PROBABILITY) {
+            return 2;
+        } else if (randomValue < COMMON_PROBABILITY + ELITE_PROBABILITY + LIMITED_PROBABILITY) {
+            return 3;
+        } else if (randomValue < COMMON_PROBABILITY + ELITE_PROBABILITY + LIMITED_PROBABILITY + SP_PROBABILITY) {
+            return 4;
+        }
+        return 0;
+    }
+}
