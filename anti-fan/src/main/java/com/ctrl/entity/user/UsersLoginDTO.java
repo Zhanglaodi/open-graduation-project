142abf1820ee4b7f5f139c09a5126d282fb51ea2
zxt <1535852024@qq.com>
2023-05-19 08:30:32 +0800

    添加用户登录dto

--- anti-fan/src/main/java/com/ctrl/entity/user/UsersLoginDTO.java
+++ anti-fan/src/main/java/com/ctrl/entity/user/UsersLoginDTO.java
@@ -2,9 +2,9 @@ package com.ctrl.entity.user;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -33,4 +33,11 @@ public class UsersLoginDTO implements Serializable {
     @NotEmpty(message = "验证码不能为空")
     @ApiModelProperty("verificationCode")
     private String verificationCode;
+
+    /**
+     * loginType
+     */
+    @NotNull(message = "登陆方式不能为空")
+    @ApiModelProperty("loginType")
+    private Integer loginType;
 }
