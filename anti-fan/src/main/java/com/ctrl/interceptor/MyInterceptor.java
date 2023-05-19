b7e392d03e9abc6ba66e02f5e7fb2d36349f8e78
zxt <1535852024@qq.com>
2023-05-19 08:32:56 +0800

    请求执行时间的拦截器

---
+++ anti-fan/src/main/java/com/ctrl/interceptor/MyInterceptor.java
@@ -0,0 +1,52 @@
+package com.ctrl.interceptor;
+
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 我拦截器
+ *
+ * @author 张宪泰
+ * @date 2023 /03/28
+ */
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+
+    private final NamedThreadLocal<Long> threadLocal = new NamedThreadLocal<>("PerformanceMonitor");
+
+    /**
+     * 前处理
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param handler  处理程序
+     * @return boolean
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        long startTime = System.currentTimeMillis();
+        threadLocal.set(startTime);
+        return true;
+    }
+
+    /**
+     * @param request  请求
+     * @param response 响应
+     * @param handler  处理程序
+     * @param ex       异常
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        long endTime = System.currentTimeMillis();
+        long startTime = threadLocal.get();
+        long elapse = endTime - startTime;
+        if (elapse > 0) {
+            System.out.printf("%s elapse %d 毫秒%n", request.getRequestURI(), elapse);
+        }
+
+    }
+}
