903595084ea0f453fe146406f756b26f2e59c0b1
zxt <1535852024@qq.com>
2023-05-19 08:27:22 +0800

    配置拦截器以及配置跨域

--- anti-fan/src/main/java/com/ctrl/config/CommonConfig.java
+++ anti-fan/src/main/java/com/ctrl/config/CommonConfig.java
@@ -1,6 +1,15 @@
 package com.ctrl.config;
 
+import com.ctrl.interceptor.AuthInterceptor;
+import com.ctrl.interceptor.MyInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * The type Common config.
@@ -8,5 +17,39 @@ import org.springframework.context.annotation.Configuration;
  * @author dalaodi
  */
 @Configuration
-public class CommonConfig {
+public class CommonConfig implements WebMvcConfigurer {
+    private static final List<String> EXCLUDE_PATH = Arrays.asList("/", "/upload/**", "/css/**", "/js/**", "/img/**", "/media/**", "/vendors/**", "/favicon.ico", "/false", "/404_not_found");
+    /**
+     * The My interceptor.
+     */
+    @Resource
+    MyInterceptor myInterceptor;
+
+
+    /**
+     * The Auth interceptor.
+     */
+    @Resource
+    AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).
+                addPathPatterns("/**").
+                excludePathPatterns("/api/login/get_captcha").excludePathPatterns(EXCLUDE_PATH);
+        registry.addInterceptor(authInterceptor)
+                .excludePathPatterns("/api/login/get_captcha").excludePathPatterns(EXCLUDE_PATH);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //放行哪些原始域
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
 }
