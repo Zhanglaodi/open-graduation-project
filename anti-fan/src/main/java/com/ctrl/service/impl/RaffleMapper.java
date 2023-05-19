96fca081a0266068f71af02b3915cc1c7b772bfc
zxt <1535852024@qq.com>
2023-05-19 08:35:46 +0800

    抽卡mapper

---
+++ anti-fan/src/main/java/com/ctrl/service/impl/RaffleMapper.java
@@ -0,0 +1,12 @@
+package com.ctrl.service.impl;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ctrl.entity.raffle.RaffleD0;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * The interface Raffle mapper.
+ */
+@Mapper
+public interface RaffleMapper extends BaseMapper<RaffleD0> {
+}
