9dc023c4f4b9da8bc91cd7939db45a12cedf2c60
zxt <1535852024@qq.com>
2023-05-19 08:29:44 +0800

    添加抽卡实体类

---
+++ anti-fan/src/main/java/com/ctrl/entity/raffle/RaffleD0.java
@@ -0,0 +1,91 @@
+package com.ctrl.entity.raffle;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ctrl.entity.user.UsersDO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+
+/**
+ * The type Raffle d 0.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+@TableName(value = "anti_fan_raffle")
+public class RaffleD0 implements Serializable {
+
+    /**
+     * The interface Add method.
+     */
+    public interface AddMethod {
+
+    }
+
+    /**
+     * The interface Edit method.
+     */
+    public interface EditMethod {
+
+    }
+
+    /**
+     * id
+     */
+    @Null(message = "新增时id必须为空", groups = {RaffleD0.AddMethod.class})
+    @NotNull(message = "更新时id不能为空", groups = {RaffleD0.EditMethod.class})
+    @ApiModelProperty("ID")
+    private Integer id;
+
+    /**
+     * 角色名称
+     */
+    @Size(max = 255, message = "编码长度不能超过255")
+    @ApiModelProperty("用户名")
+    @JsonProperty("user_name")
+    @NotBlank(message = "用户名称不能为空", groups = {RaffleD0.AddMethod.class})
+    private String characterName;
+
+    /**
+     * 卡牌等级
+     */
+    @ApiModelProperty("卡牌等级")
+    @NotBlank(message = "用户名称不能为空", groups = {RaffleD0.AddMethod.class})
+    private Integer level;
+
+    /**
+     * 海报
+     */
+    @Size(max = 255, message = "编码长度不能超过255")
+    @ApiModelProperty("图片地址")
+    private String postImg;
+
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty("创建时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private String createTime;
+
+    /**
+     * 更新时间
+     */
+    @ApiModelProperty("更新时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @TableField(fill = FieldFill.UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private String updateTime;
+}
