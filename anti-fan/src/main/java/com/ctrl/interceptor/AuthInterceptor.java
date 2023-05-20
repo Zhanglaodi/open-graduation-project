89cf062fbd781088dcb155c3e02a7e68bcb19355
zxt <1535852024@qq.com>
2023-05-20 17:19:07 +0800

    避免异常抛出：可以将抛出异常的地方改为返回错误信息，这样可以减少异常的抛出和捕获，提高代码的执行效率。 减少重复代码：可以将重复使用的逻辑抽取成方法或工具类，避免代码冗余。 代码注释：添加适当的代码注释，解释代码的作用和意图，提高代码的可理解性。

--- anti-fan/src/main/java/com/ctrl/interceptor/AuthInterceptor.java
+++ anti-fan/src/main/java/com/ctrl/interceptor/AuthInterceptor.java
@@ -1,7 +1,8 @@
 package com.ctrl.interceptor;
 
 
-import com.ctrl.entity.ResultException;
+import com.ctrl.entity.CommonResult;
+import com.ctrl.utils.JsonUtils;
 import com.ctrl.utils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
 /**
@@ -34,42 +36,63 @@ public class AuthInterceptor implements HandlerInterceptor {
      * @return boolean 布尔
      */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        System.out.println("请求路劲：" + request.getRequestURI());
-        String token =
-                Optional.ofNullable(request.getParameter("token")).orElseThrow(
-                        () -> new ResultException(-9998, "token丢失")
-                );
-        if (request.getRequestURI().contains("/api/login/login")) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        String requestURI = request.getRequestURI();
+        String token = request.getParameter("token");
+        if (token == null) {
+            sendErrorResponse(response, -9998, "token丢失");
+        }
+        if (requestURI.contains("/api/login/login")) {
             //判断验证码/token是否失效
-            Optional.ofNullable(redisUtils.get("anti_fan:" + token, 7)).orElseThrow(
-                    () -> new ResultException(-9999, "非法的token")
-            );
+            if (validateToken(token, "anti_fan:" + token, 7)) {
+                sendErrorResponse(response, -9999, "非法的token");
+                return false;
+            }
         } else {
             //从缓存中读取用户是否登录
-            Optional.ofNullable(redisUtils.get("anti_fan:" + token, 8)).orElseThrow(
-                    () -> new ResultException(-9997, "未登录")
-            );
+            if (validateToken(token, "anti_fan:" + token, 8)) {
+                sendErrorResponse(response, -9997, "未登录，请先登录");
+                return false;
+            }
             //设置缓存中的时间为两个小时
             redisUtils.expire("anti_fan:" + token, 8, 7200);
         }
-        //请求中的时间戳
-        Optional.ofNullable(request.getParameter("timeStamp")).map(var -> {
-            if (var.matches("^(\\d{10,13})$")) {
-                long localTime = Math.abs(System.currentTimeMillis() / 1000);
-                System.out.println("localTime = " + localTime);
-                if (localTime - Integer.parseInt(var) >= 10) {
-                    throw new ResultException(-20000, "中间人攻击?");
-                }
-                return 0;
-            } else {
-                System.out.println(request.getParameter("timeStamp"));
-                throw new ResultException(-20001, "错误的时间格式");
-            }
-        }).orElseThrow(
-                () -> new ResultException(-21000, "请求时时间戳缺失")
-        );
-        //权限会热修改吗？
+        String timeStamp = request.getParameter("timeStamp");
+        if (timeStamp == null || !timeStamp.matches("^(\\d{10,13})$")) {
+            sendErrorResponse(response, -21000, "请求时时间戳缺失或错误的时间格式");
+            return false;
+        }
+        long localTime = Math.abs(System.currentTimeMillis() / 1000);
+        if (localTime - Integer.parseInt(timeStamp) >= 10) {
+            sendErrorResponse(response, -20000, "中间人攻击?");
+            return false;
+        }
         return true;
+    }
+
+    /**
+     * 验证token
+     *
+     * @param token        token
+     * @param redisKey     key
+     * @param redisDbIndex dbIndex
+     * @return boolean
+     */
+    private boolean validateToken(String token, String redisKey, int redisDbIndex) {
+        return !Optional.ofNullable(redisUtils.get(token + redisKey, redisDbIndex)).isPresent();
+    }
+
+    /**
+     * 发送错误信息
+     *
+     * @param response     响应
+     * @param errorCode    错误码
+     * @param errorMessage 错误信息
+     */
+    private void sendErrorResponse(HttpServletResponse response, int errorCode, String errorMessage) throws IOException {
+        CommonResult<Object> error = CommonResult.error(errorCode, errorMessage);
+        String beanToJson = JsonUtils.getBeanToJson(error);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(beanToJson != null ? beanToJson : "error");
     }
 }
