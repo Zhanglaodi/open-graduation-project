f4ddd7572a4dfc240b97796a18ab14ffb5ee7aab
zxt <1535852024@qq.com>
2023-05-23 10:03:11 +0800

    添加新的类RaffleVO

---
+++ anti-fan/src/main/java/com/ctrl/entity/raffle/RaffleVO.java
@@ -0,0 +1,15 @@
+package com.ctrl.entity.raffle;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * The type Raffle vo.
+ */
+@Data
+public class RaffleVO implements Serializable {
+    private String characterName;
+    private String level;
+    private String headImg;
+}
