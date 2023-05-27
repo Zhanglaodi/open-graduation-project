3e0af7152dc373e4b00eae6cf41cf1737ffadaf4
zxt <1535852024@qq.com>
2023-05-27 17:17:38 +0800

    卡牌技能

---
+++ anti-fan/src/main/java/com/ctrl/entity/raffle/RaffleSkills.java
@@ -0,0 +1,33 @@
+package com.ctrl.entity.raffle;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class RaffleSkills {
+    private String name;
+    private String description;
+    /**
+     * 卡牌基础防御
+     */
+    @ApiModelProperty("卡牌基础防御")
+    private Integer baseDefense;
+
+    /**
+     * 基础力量
+     */
+    @ApiModelProperty("卡牌基础防御")
+    private Integer baseStrength;
+
+    /**
+     * 基础速度
+     */
+    @ApiModelProperty("卡牌基础速度")
+    private Integer baseSpeed;
+
+    /**
+     * 基础血量
+     */
+    @ApiModelProperty("卡牌基础血量")
+    private Integer baseHealth;
+}
