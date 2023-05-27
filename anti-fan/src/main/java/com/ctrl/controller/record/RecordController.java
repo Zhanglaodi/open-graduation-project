55d59d389dc75e8ef5f2b56b8ea38c1b3c2d80f6
zxt <1535852024@qq.com>
2023-05-27 17:21:47 +0800

    测试接口

---
+++ anti-fan/src/main/java/com/ctrl/controller/record/RecordController.java
@@ -0,0 +1,24 @@
+package com.ctrl.controller.record;
+
+import com.ctrl.entity.CommonResult;
+import com.ctrl.entity.CountAndData;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * The type Record controller.
+ */
+@RestController
+@RequestMapping(value = "/api/record")
+public class RecordController {
+    @PostMapping(value = "/queryList")
+    public CommonResult<List<String>> queryList(
+    ) {
+
+        CountAndData<List<String>> a = null;
+        return CommonResult.ok("查询成功", a);
+    }
+}
