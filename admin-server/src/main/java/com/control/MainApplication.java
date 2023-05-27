fb11a1a4eb45d8fd30967f8440d8c5a198da3101
zxt <1535852024@qq.com>
2023-05-27 17:12:50 +0800

    添加注释

--- admin-server/src/main/java/com/control/MainApplication.java
+++ admin-server/src/main/java/com/control/MainApplication.java
@@ -4,9 +4,17 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Main application.
+ */
 @SpringBootApplication
 @EnableAdminServer
 public class MainApplication {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
