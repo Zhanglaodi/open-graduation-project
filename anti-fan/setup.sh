453df805940b35e8957821c299957b8d22bd7b78
zxt <1535852024@qq.com>
2023-05-27 17:22:39 +0800

    修改运行脚本

--- anti-fan/setup.sh
+++ anti-fan/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /opt/src
 
-java  -Djava.security.egd=file:/dev/./urandom -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1005 -jar app.jar
+java -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=56m -Xms128m -Xmx128m -Xmn32m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC  -Djava.security.egd=file:/dev/./urandom -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1005 -jar app.jar
